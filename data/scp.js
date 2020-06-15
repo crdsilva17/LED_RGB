11d5d4486acb7171b999cc8b65bb2c4c525f965d
Cristiano Rocha <cristianoatmcrs@gmail.com>
2020-06-15 00:24:03 -0300

    pump set

--- data/scp.js
+++ data/scp.js
@@ -1,23 +1,61 @@
 
+function sendCor(){
 
-function trocaCor(){
-    var r = document.getElementById("red").value;
-    var g = document.getElementById("green").value;
-    var b = document.getElementById("blue").value;
     var xh = new XMLHttpRequest();
-    xh.open("GET","/update", true);
+    xh.open("POST","/update", true);
     xh.onreadystatechange = function() { // Chama a função quando o estado mudar.
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
             // Requisição finalizada. Faça o processamento aqui.
         }
     }
-    xh.send("R="+r+"&G="+g+"&B="+b); 
-    r = Number(r).toString(16);
-    g = Number(g).toString(16);
-    b = Number(b).toString(16);
-document.getElementById("menu").style.backgroundColor = "#" + r + "f" + g + "f" + b + "f";
+
+var cor = document.getElementById("selector").value;
+var R = parseInt(cor.substring(1,3),16);
+var G = parseInt(cor.substring(3,5),16);
+var B = parseInt(cor.substring(5,8),16);
+var params = new FormData();
+params.append("R",R);
+params.append("G",G);
+params.append("B",B);
+console.log("RGB(",R,",",G,",",B,")");
+xh.send(params);
 }
 
+function pumpSet(param1, param2){
+    var xh = new XMLHttpRequest();
+    xh.open("POST","/update", true);
+    xh.onreadystatechange = function(){ // chama a função quando o estado mudar.
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            //Requisição finalizada. Faca o processamento aqui.
+        }
+    } 
+    var params = new FormData();
+    params.append(param1, param2);
+    xh.send(params);
+    console.log(param1,": ", param2);
+}
 
+function init(){
+document.getElementById("selector").addEventListener("input",sendCor);
+document.querySelector("#pump").addEventListener("change",function(envent){
+    var checkePump = document.querySelector("#pump");
+    if (checkePump.checked) {
+        pumpSet("pump","on");
+    }else{
+        pumpSet("pump","off");
+    }
+});
+document.querySelector("#auto").addEventListener("change",function(envent){
+    var auto = document.querySelector("#auto");
+    var selector = document.querySelector("#selector");
+    if (auto.checked) {
+        selector.disabled = true;
+        pumpSet("color","auto");
+    }else{
+        selector.disabled = false;
+        pumpSet("color","handle");
+    }
+});
+}
 
-
+document.addEventListener("DOMContentLoaded",init,false);
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <FS.h>
 #include <LittleFS.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPUpdateServer.h>
 
 #define SSID  "Virus" // Ssid Network
 #define PASSWD  "aoc18090" // Password for Network
+#define pump   D8 // define pin of pump
+#define sunLight D4 // define ldr sensor
+
+const char* user = "admin"; //username
+const char* pass = "senha123"; //password
+const char* host = "aqua"; //hostname - aqua.local
+const char* update_path = "/firmware";
+
+bool hand = true;
+long t = 0L;
+
+File uploadFile;
+
+
 
 int pinR = 12; //pin red color
 int pinG = 13; //pin green color
@@ -14,9 +31,9 @@ int pinB = 14; //pin blue color
 String page = ""; // variable that we can set the html page
 
 ESP8266WebServer serverA(80); //Instance Server
+ESP8266HTTPUpdateServer httpUpdater;
 
-const char* user = "admin"; //username
-const char* pass = "senha123"; //password
+
 
 void handleRoot();
 void handleCss();
@@ -33,9 +50,15 @@ void setup() {
   pinMode(pinR, OUTPUT); // Set pin as Output
   pinMode(pinG, OUTPUT); // Set pin as Output
   pinMode(pinB, OUTPUT); // Set pin as Output
+  pinMode(pump, OUTPUT);// Set  pin pump as Output
+  pinMode(sunLight, INPUT); // Set pin sensor sun as Input
 
-  analogWriteRange(8); // Range 0 at 255
-  analogWriteFreq(5000); // Set Frequency
+  digitalWrite(pump, LOW); // Set pump off
+
+
+  analogWriteRange(10); // Range 0 at 1023
+  analogWriteFreq(60); // Set Frequency
+
 
   analogWrite(pinR,0); // pin off
   analogWrite(pinG,0); // pin off
@@ -57,21 +80,109 @@ void setup() {
 
   serverA.on("/st.css",handleCss); // trata chamadas para folha de estilo
   serverA.on("/scp.js",handleJs); // trata chamadas para script
-  serverA.on("/update",HTTP_GET,handleAction); // trata chamadas para ação de acionamento do led
-  serverA.onNotFound(handleNotFound); // trata erro de paginas
-  
+  serverA.on("/update",handleAction); // trata chamadas para ação de acionamento do led
+  serverA.on("/favicon.ico",HTTP_GET,[](){
+  page = readFile("/favicon.ico");
+  serverA.send(200,"image/x-icon",page);
+    
+    });
+  serverA.onNotFound(handleNotFound);// trata erro de paginas
 
+  
+  MDNS.begin(host);
+  httpUpdater.setup(&serverA, update_path, user, pass);
   serverA.begin();
+
+  MDNS.addService("http", "tcp", 80);
+  
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   serverA.handleClient();
+  MDNS.update();
+  if(!hand){
+    if(t > 1000L){t = 0L;}
+    analogWrite(pinR, 255);
+    analogWrite(pinG, 0);
+    analogWrite(pinB, 0);
+    delay(t);
+    analogWrite(pinR, 0);
+    analogWrite(pinG, 255);
+    analogWrite(pinB, 0);
+    delay(t);
+    analogWrite(pinR, 0);
+    analogWrite(pinG, 0);
+    analogWrite(pinB, 255);
+    delay(t);
+    analogWrite(pinR, 255);
+    analogWrite(pinG, 255);
+    analogWrite(pinB, 0);
+    delay(t);
+    analogWrite(pinR, 255);
+    analogWrite(pinG, 0);
+    analogWrite(pinB, 255);
+    delay(t);
+    analogWrite(pinR, 0);
+    analogWrite(pinG, 255);
+    analogWrite(pinB, 0);
+    delay(t);
+    analogWrite(pinR, 0);
+    analogWrite(pinG, 255);
+    analogWrite(pinB, 255);
+    delay(t);
+    analogWrite(pinR, 255);
+    analogWrite(pinG, 10);
+    analogWrite(pinB, 0);
+    delay(t);
+    analogWrite(pinR, 255);
+    analogWrite(pinG, 0);
+    analogWrite(pinB, 10);
+    delay(t);
+    analogWrite(pinR, 255);
+    analogWrite(pinG, 10);
+    analogWrite(pinB, 10);
+    delay(t);
+    analogWrite(pinR, 10);
+    analogWrite(pinG, 255);
+    analogWrite(pinB, 0);
+    delay(t);
+    analogWrite(pinR, 0);
+    analogWrite(pinG, 255);
+    analogWrite(pinB, 10);
+    delay(t);
+    analogWrite(pinR, 10);
+    analogWrite(pinG, 255);
+    analogWrite(pinB, 10);
+    delay(t);
+    analogWrite(pinR, 10);
+    analogWrite(pinG, 0);
+    analogWrite(pinB, 255);
+    delay(t);
+    analogWrite(pinR, 0);
+    analogWrite(pinG, 10);
+    analogWrite(pinB, 255);
+    delay(t);
+    analogWrite(pinR, 10);
+    analogWrite(pinG, 10);
+    analogWrite(pinB, 255);
+    delay(t);
+    analogWrite(pinR, 255);
+    analogWrite(pinG, 255);
+    analogWrite(pinB, 255);
+    delay(t);
+    t++;
+    }
+
 }
 
 String readFile(String path){ // Função de leitura dos arquivos na memória flash
    String content = ""; // cria variavel de retorno
+   LittleFSConfig cfg;
+   cfg.setAutoFormat(false);
+   LittleFS.setConfig(cfg);
+   
    if(LittleFS.begin()){ // inicia classe de arquivo
     if(LittleFS.exists(path)){ // verifica se o arquivo solicitado existe
     File file = LittleFS.open(path,"r"); // abre o arquivo em modo de leitura
@@ -106,13 +217,35 @@ void handleNotFound(){ // Função que indica que a pagina web não foi encontra
  Função que trata as ações enviadas pelo cliente
 */
 void handleAction(){
+
+  if(hand){
   if(serverA.argName(0).equals("R")){
-    analogWrite(pinR,serverA.arg(0).toInt()); //Aciona o PWM para a saída Vermelha
+    analogWrite(pinR,map(serverA.arg(0).toInt(),0,255,0,1023)); //Aciona o PWM para a saída Vermelha
   }
   if(serverA.argName(1).equals("G")){
-    analogWrite(pinG,serverA.arg(1).toInt()); // Aciona o PWM para a saída Verde
+    analogWrite(pinG,map(serverA.arg(1).toInt(),0,255,0,1023)); // Aciona o PWM para a saída Verde
   }
   if(serverA.argName(2).equals("B")){
-    analogWrite(pinB,serverA.arg(2).toInt()); // Aciona o PWM para a saída Azul
+    analogWrite(pinB,map(serverA.arg(2).toInt(),0,255,0,1023)); // Aciona o PWM para a saída Azul
   }
+  }
+
+  // On Off Pump
+  if(serverA.argName(0).equals("pump")){
+    if(serverA.arg(0).equals("on")){
+      digitalWrite(pump,HIGH);
+    }else if(serverA.arg(0).equals("off")){
+      digitalWrite(pump,LOW);
+    }
+  }
+
+  if(serverA.argName(0).equals("color")){
+    if(serverA.arg(0).equals("auto")){
+      hand = false;
+    }else if(serverA.arg(0).equals("handle")){
+      hand = true;
+    }
+  }
+  serverA.send(200);
 }
+
